6187b1bc33074c82c914f82b1ac9ecdc9e216799
Sören Metje <soerenmetje@yahoo.de>
2023-03-10 00:48:42 +0100

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,38 @@
+# Kubernetes Cluster based on Singularity
+
+Set up a Kubernetes cluster using Singularity as the container runtime.
+
+For this, a container runtime interface (CRI) for Singularity is needed. It adapts the Singularity container runtime in
+a standardized way and therefore enables Kubernetes to use it. A (deprecated) implementation is available here: https://github.com/sylabs/singularity-cri
+
+Ultimately, this should help us run Kubernetes workloads on HPC clusters.
+
+## Setup
+
+Install and set up Singularity, Singularity-CRI and Kubernetes:
+
+``` shell
+/bin/bash ./setup.sh
+```
+> So far only tried as root user on Ubuntu 22.04
+
+## Notes
+
+- In general, Kubernetes and Singularity continued evolving while Singularity-CRI was archived in December 2020
+- If one tries to use Singularity-CRI with current implementation of Kubernetes and Singularity, multiple errors occur,
+  so no functioning state could be achieved (March 2023 - Kubernetes 1.26 and SingularityCE 3.11.0)
+    - Error when init Kubernetes cluster
+    - Warning after starting Singularity-CRI service
+      (we can prob. ignore this for now)
+- If one tries to use old implementations from the time of the last Singularity-CRI release,
+  same as with current implementation
+
+![img.png](images/errors1.png)
+
+## Sources
+
+- https://slateci.io/blog/kubernetes-with-singularity.html
+- https://docs.sylabs.io/guides/cri/1.0/user-guide/k8s.html
+- https://github.com/sylabs/singularity-cri
+- https://github.com/sylabs/wlm-operator/tree/master/vagrant
+- https://github.com/sylabs/singularity
---
+++ setup.sh
@@ -0,0 +1,201 @@
+#!/bin/sh
+
+# Set up a Kubernetes cluster that uses Singularity as container runtime.
+
+set -x # Print each command before execution
+set -e # fail and abort script if one command fails
+set -o pipefail
+
+# Install Singularity ==================================================================================
+# https://docs.sylabs.io/guides/3.8/admin-guide/installation.html#installation-on-linux
+
+# Install Dependencies --------------------------------------------------
+# ... for rhel-based linux
+if [ -x "$(which yum)" ]; then
+  sudo yum update -y &&
+    sudo yum groupinstall -y 'Development Tools' &&
+    sudo yum install -y \
+      openssl-devel \
+      libuuid-devel \
+      libseccomp-devel \
+      wget \
+      squashfs-tools \
+      cryptsetup \
+      inotify-tools \
+      git \
+      nano
+
+# ... for debian-based linux
+elif [ -x "$(which apt)" ]; then
+  export DEBIAN_FRONTEND=noninteractive &&
+    sudo -E apt update &&
+    sudo -E apt install -y \
+      build-essential \
+      libssl-dev \
+      uuid-dev \
+      libgpgme11-dev \
+      libseccomp-dev \
+      pkg-config \
+      squashfs-tools \
+      inotify-tools \
+      git
+else
+  echo "Error: No supported package manager installed (yum or apt)" >&2 && exit 1
+fi
+
+# Install Go ----------------------------------------------------------
+
+wget https://go.dev/dl/go1.20.1.linux-amd64.tar.gz
+
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
+
+# shellcheck disable=SC2016
+echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile
+
+# reload PATH var to make go command available without new login
+source /etc/profile
+
+go version
+
+# Install Singularity --------------------------------------------------
+
+if [ -d singularity ]; then
+  rm -r singularity # clean up
+fi
+
+# See Singularity releases: https://github.com/sylabs/singularity/releases
+
+# > 3.7.3 following syntax
+#export VERSION=3.8.4 && # adjust this as necessary \
+#  wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-ce-${VERSION}.tar.gz &&
+#  tar -xzf singularity-ce-${VERSION}.tar.gz &&
+#  cd singularity-ce-${VERSION}
+
+# <= 3.7.3 following syntax
+export VERSION=3.7.3 && # adjust this as necessary \
+  wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz &&
+  tar -xzf singularity-${VERSION}.tar.gz &&
+  cd singularity
+
+# Compile
+./mconfig &&
+  make -C ./builddir &&
+  sudo make -C ./builddir install
+
+# enjoy bash shell completion with SingularityCE commands and options
+. /usr/local/etc/bash_completion.d/singularity
+
+cd ..
+
+# Install SingularityCRI ==================================================================================
+# Source: https://docs.sylabs.io/guides/cri/1.0/user-guide/installation.html
+
+# See Singularity-CRI releases: https://github.com/sylabs/singularity-cri/tags
+export CRI_VERSION=1.0.0-beta.7 # adjust this as necessary
+
+if [ -d singularity-cri ]; then
+  rm -r singularity-cri # clean up
+fi
+
+if [ -d singularity-cri-${CRI_VERSION} ]; then
+  rm -r singularity-cri-${CRI_VERSION} # clean up
+fi
+
+wget -O singularity-cri-${CRI_VERSION}.tar.gz https://github.com/sylabs/singularity-cri/archive/refs/tags/v${CRI_VERSION}.tar.gz &&
+  tar -xzf singularity-cri-${CRI_VERSION}.tar.gz &&
+  mv singularity-cri-${CRI_VERSION} singularity-cri
+
+cd singularity-cri
+
+go mod vendor &&
+  make &&
+  sudo make install
+
+# Setup SingularityCRI Integrating with Kubernetes ==================================================================================
+# Source: https://docs.sylabs.io/guides/cri/1.0/user-guide/k8s.html
+
+cat >/etc/systemd/system/sycri.service <<EOT
+[Unit]
+Description=Singularity-CRI
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=30
+User=root
+Group=root
+ExecStart=${PWD}/bin/sycri -v 2
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+sudo systemctl enable sycri &&
+  sudo systemctl start sycri
+
+sudo systemctl status sycri --no-pager -l
+
+cd ..
+
+# Install Kubernetes
+
+# Disable SELinux
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+# configure system network config
+sudo modprobe br_netfilter
+sudo sysctl -w net.bridge.bridge-nf-call-iptables=1
+sudo sysctl -w net.ipv4.ip_forward=1
+
+# See Kubernetes releases: https://kubernetes.io/releases/
+export KUBE_VERSION=1.16.15-00
+
+# ... for rhel-based linux
+if [ -x "$(which yum)" ]; then
+  cat >/etc/yum.repos.d/kubernetes.repo <<EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg	 https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+
+  yum install -y kubelet-${KUBE_VERSION} kubeadm-${KUBE_VERSION} kubectl-${KUBE_VERSION} --disableexcludes=kubernetes
+  systemctl enable kubelet
+
+# ... for debian-based linux
+elif [ -x "$(which apt)" ]; then
+
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+  sudo apt-add-repository -y "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+  sudo -E apt-get install -y kubelet=${KUBE_VERSION} kubeadm=${KUBE_VERSION} kubectl=${KUBE_VERSION}
+else
+  echo "Error: No supported package manager installed (yum or apt)" >&2 && exit 1
+fi
+
+# Setup Kubernetes Cluster
+
+cat >/etc/default/kubelet <<EOF
+KUBELET_EXTRA_ARGS=--container-runtime=remote \
+--container-runtime-endpoint=unix:///var/run/singularity.sock \
+--image-service-endpoint=unix:///var/run/singularity.sock
+EOF
+
+sudo systemctl restart kubelet
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+kubeadm init --pod-network-cidr=192.168.0.0/16 --cri-socket unix:///var/run/singularity.sock
+# FIXME kubeadm init not working
+
+# TODO add testing kubernetes
+
+# TODO add getting kube config
